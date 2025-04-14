bd9c9c68b20eb17cdc2448817d0599b2b84f0630
wangchengtao <915129420@qq.com>
2025-04-14 15:11:37 +0800

    bug fixed

--- tests/Units/AopClientTest.php
+++ tests/Units/AopClientTest.php
@@ -26,22 +26,7 @@ class AopClientTest extends TestCase
         $this->config = new Config(
             'https://openapi-test.tianquetech.com',
             '14653730',
-            '-----BEGIN PRIVATE KEY-----
-MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBAObo+KhAY6nBfikP
-XeLKHRgOfzjrmtckXCbSBuILTnIlVGIop5J0zkzfcCLzwk+xEij6c/t5vqFmpPNu
-1562HspzrpnMHF8ieTwKPR7JSKKqlOs78ApewkSN80ob0v0y7Ni8bMnhGP63Q9AB
-o5f1wd64t8IUuE2KG0gXJDQA9HAzAgMBAAECgYBkj18C/w+oNSD5RrzvcG/dMysK
-oeiL+0S6j0r6FEx0f3dRTD02FfJmHgSa5CdiR04OVIxAH1P6vFXnwgTbFJsxYgx9
-65G8q7tM6qVs2qt2XggGshcOdjunG2x4T44eJMwxJrDlTT/CIumihsVRd8ImdTV7
-GhMZKoCDO/CxikvxiQJBAPRXy9ZyrM5eJK0DRx1n8fW25KIVOtJFjq0cAm08HVbU
-wZm93UE9UXNs7TLZUSsrK6UM2Uv+rdHi/ZLwym7gi98CQQDx7R3XhzvBfWH9xyKl
-pBnA5FV104XDPHxEIz7bEMX7vFUSQCsYmMKv7vu0IAOr6EH5/QOJB88wqC39Okdc
-X2YtAkAn1GPy2hCXNzttRHqELZyAfEa6sRE8k4AVcdpnagQyUk4YvJ1jdBZh5WCp
-CEm16ryblAOb4rD85K6HFF87QbkrAkEArpzcOyGqaZ9byNWgFjn5NJYZcK+5Dg9s
-Can+xhK3M1jddgzGjjxD2MP+/CVXQQ6kABE0KgVu78mTWABmXS+mCQJBALoKAAhB
-QG3xFVv5lJqgf+jzWWWdcdw1fzqnp+yhafrlhWd6serV3LGftcbTNtivIS4DNAAK
-bUNl3XlBBUFiODI=
------END PRIVATE KEY-----',
+            file_get_contents( TEST_ROOT .'/resources/private_key.pem'),
             '-----BEGIN PUBLIC KEY-----
 MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCOmsrFtFPTnEzfpJ/hDl5RODBxw4i9Ex3NmmG/N7A1+by032zZZgLLpdNh8y5otjFY0E37Nyr4FGKFRSSuDiTk8vfx3pv6ImS1Rxjjg4qdVHIfqhCeB0Z2ZPuBD3Gbj8hHFEtXZq8+msAFu/5ZQjiVhgs5WWBjh54LYWSum+d9+wIDAQAB
 -----END PUBLIC KEY-----',
