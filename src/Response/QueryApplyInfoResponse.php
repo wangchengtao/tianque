3eb9f3648533037652bba9027f5471c8b9c89bc5
wangchengtao <915129420@qq.com>
2026-05-26 09:26:22 +0800

    商户特殊申请查询添加注释

--- src/Response/QueryApplyInfoResponse.php
+++ src/Response/QueryApplyInfoResponse.php
@@ -14,24 +14,55 @@ class QueryApplyInfoResponse extends Response
 
     public const CANCELLED = '03'; // 申请取消
 
+    /**
+     * 申请单号.
+     */
     public string $id;
 
+    /**
+     * 申请类型，枚举：
+     * 1 分时结算申请
+     * 2 订单分账申请.
+     */
     public string $applicationType;
 
     public string $mno;
 
+    /**
+     * 申请状态，枚举值：
+     * 00 申请审核中
+     * 01 申请通过
+     * 02 申请驳回
+     * 03 申请取消.
+     */
     public string $applyStatus;
 
-    public string $handleExplain;
+    /**
+     * 处理说明.
+     */
+    public string $handleExplain = '';
 
     // --------------------------------------------------- 分时结算申请
 
+    /**
+     * 定时切批付款设置，枚举值：
+     * 0 当日付款
+     * 1 次日付款.
+     */
     public string $paymentSet;
 
+    /**
+     * 切批时间
+     * 当日付款：01-12 整数
+     * 次日付款：00-23整数.
+     */
     public string $settTime;
 
-    // ---------------------------------------------------- 账户结算申请
+    // ---------------------------------------------------- 订单分账申请
 
+    /**
+     * 商户最大分账比例.
+     */
     public string $accountRatio;
 
     public function getId(): string
